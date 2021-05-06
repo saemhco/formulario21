76682cb077badc3d0a1ef5a6a3c05372c4e6432e
saemhco <saulem1994@gmail.com>
2021-05-05 22:08:09 -0500

    ..

--- resources/views/welcome/formulario.blade.php
+++ resources/views/welcome/formulario.blade.php
@@ -10,11 +10,11 @@
             <table class="table">
                 <tr>
                     <td align="right"> <input class="form-check-input" type="radio" name="dia" value="1" id="dia_1" style="height:25px; width:35px;vertical-align: middle;" >   </td>
-                    <td align="left"> <label class="form-check-label text-white" for="dia_1" style="font-size:20px;">FECHA 1:  30 de marzo 2021</label></td>
+                    <td align="left"> <label class="form-check-label text-white" for="dia_1" style="font-size:20px;">FECHA 1: 11  de mayo 2021</label></td>
                 </tr>
                 <tr>
                     <td align="right"> <input class="form-check-input" type="radio" name="dia" value="2" id="dia_2" style="height:25px; width:35px;vertical-align: middle; "> </td>
-                    <td align="left"><label class="form-check-label text-white" for="dia_2" style="font-size:20px;">FECHA 2: 31 de marzo 2021</label></td>
+                    <td align="left"><label class="form-check-label text-white" for="dia_2" style="font-size:20px;">FECHA 2: 12 de mayo 2021</label></td>
                 </tr>
             </table>
             <button type="button" class="btn btn-block btn-success" onclick="enviar()">Enviar</button>
